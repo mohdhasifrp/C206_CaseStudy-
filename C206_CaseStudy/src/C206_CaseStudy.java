1522075ccaf36c5917b7d5e0ebe3d1241105c536
19003063 <19003063@BL4CKHASIF>
2020-08-20 23:53:59 +0800

    test

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,9 +1,33 @@
 
+import java.util.ArrayList;
 public class C206_CaseStudy {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int option = 0;
 
+		while (option != 5) {
+
+		C206_CaseStudy.menu();
+		option = Helper.readInt("Enter an option > ");
+		}
+	}
+		
+		
+	public static void menu() {
+		C206_CaseStudy.setHeader("Renovation ACE customer app");
+		System.out.println("1. Visitor account Registration");
+		System.out.println("2.Request for Quotation");
+		System.out.println("3. Manage Appointment");
+		System.out.println("5. Quit");
+		Helper.line(80, "-");
+
+	}
+	
+	public static void setHeader(String header) {
+		Helper.line(80, "-");
+		System.out.println(header);
+		Helper.line(80, "-");
 	}
 
 }
