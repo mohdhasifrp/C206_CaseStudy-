bbe102fc5fc0a3b9da4152217d5d293dbbbd0c69
19003063 <19003063@BL4CKHASIF>
2020-08-21 00:28:30 +0800

    Visitor account Registration and menu

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -10,6 +10,29 @@ public class C206_CaseStudy {
 
 		C206_CaseStudy.menu();//HASIF
 		option = Helper.readInt("Enter an option > ");
+		if (option == 1) {
+			C206_CaseStudy.setHeader("ADD USER ACCOUNT");			
+			C206_CaseStudy.setHeader("VIEW ALL USER");
+			C206_CaseStudy.setHeader("DELET USER BY NAME  ");
+			System.out.println("1. ADD USER ACCOUNT");
+			System.out.println("2. VIEW ALL USER");
+			System.out.println("3. DELET USER BY NAME");
+			int functionType = Helper.readInt("Enter option to select function > ");
+			if (functionType == 1) {
+				// ADD USER ACCOUNT
+				
+
+			} else if (functionType == 2) {
+				// VIEW ALL USER
+			
+			}else if (functionType == 3) {
+				//DELET USER BY NAME
+			} else {
+				System.out.println("Invalid functionType");
+		
+			}}else if (option == 2) {
+
+		}
 		}
 	}
 		
@@ -19,7 +42,7 @@ public class C206_CaseStudy {
 		System.out.println("1. Visitor account Registration");
 		System.out.println("2.Request for Quotation");
 		System.out.println("3. Manage Appointment");
-		System.out.println("5. Quit");
+		System.out.println("4. Quit");
 		Helper.line(80, "-");
 
 	}
